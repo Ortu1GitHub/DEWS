279545dfe26fa704296d245b5f60607c4da94248
Ortu1GitHub <jooror@hotmail.com>
2025-01-11 13:06:00 +0100

    comments changed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-                // Registrar middlewares globales o personalizados aquí
+                // Registrar middlewares aquí
                 \App\Http\Middleware\ValidateStudentId::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
